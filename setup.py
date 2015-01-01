03d242c0f354b5e3a2dbb3deb2a3b875fdd0df9e
HAYASHI Kentaro <kenhys@gmail.com>
2015-01-01 16:55:50 +0900

    Fix version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = ['Sphinx>=0.6']
 
 setup(
     name='sphinxcontrib-openstreetmap',
-    version='0.1',
+    version='0.1.0',
     url='http://bitbucket.org/birkenfeld/sphinx-contrib',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-openstreetmap',
     license='BSD',
