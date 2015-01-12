1c57b9665a8c38628087066b67cd6854db2fa48f
HAYASHI Kentaro <kenhys@gmail.com>
2015-01-12 18:57:58 +0900

    Add function to calculate xtile and ytile

--- sphinxcontrib/openstreetmap.py
+++ sphinxcontrib/openstreetmap.py
@@ -12,7 +12,18 @@ from docutils import nodes
 from docutils.parsers.rst import directives
 from sphinx.util.compat import Directive
 import csv
+import math
 
+
+def deg2num(lat_deg, lon_deg, zoom):
+    lat_rad = math.radians(lat_deg)
+    n = 2.0 ** zoom
+    xtile = int((lon_deg + 180.0) / 360.0 * n)
+    ytile = int((1.0 - 
+                 math.log(math.tan(lat_rad) +
+                          (1 / math.cos(lat_rad))) /
+                 math.pi) / 2.0 * n)
+    return (xtile, ytile)
 
 class OpenStreetMapRenderer(object):
     pass
